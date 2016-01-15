4c921a66ad9092ffa359fb6a41fee069836b063e
stuart <stutech@live.com>
2016-01-15 10:14:05 -0700

    QQ-355 -fixed validation bugs

--- source/app/controllers/import-users-controller.js
+++ source/app/controllers/import-users-controller.js
@@ -12,24 +12,45 @@ function ImportUsersController($scope, _, $state) {
     ctrl.roles =["Admin", "Manager", "Rep"];
     ctrl.letterLimit = 1;
 
-    //returns index for user then sets the user role based on button pressed
+    //returns index for user then sets the user role in array based on button pressed
     ctrl.clickedRole = function(userId, role){
     var userIndex = _.findIndex(ctrl.users, "userId", userId);
     ctrl.users[userIndex].role= role;
+    ctrl.validate(userIndex);
     };
 
     //returns array of only selected users on finish click then goes to profile page
-    ctrl.getChecked = function () {
-        var usersChecked = [];
-        for (var i = 0, l = ctrl.users.length; i < l; i++) {
-            if (ctrl.users[i].isChecked) {
-                usersChecked.push(angular.copy(ctrl.users[i]));
-                delete usersChecked[i].isChecked;
-            }
-        }
-        ctrl.usersChkd = usersChecked;
+    ctrl.finishPressed = function () {
+        //todo add logic that does something with selected users here
         $state.go('root.profile');
     };
 
+    //when user check is toggled rest selected role and add call validation
+    ctrl.checkEnabled = function(userId) {
+        //reset role if already set to enable toggle
+        var userIndex = _.findIndex(ctrl.users, "userId", userId);
+            ctrl.users[userIndex].role = "";
 
+        //filter array fo users that are selected
+        ctrl.filteredUsers = _.filter(ctrl.users, "isChecked");
+        console.log("1",ctrl.filteredUsers);
+
+            ctrl.validate(userIndex);
+    };
+
+    //
+    ctrl.validate = function(userIndex) {
+        if (ctrl.filteredUsers != "" && ctrl.filteredUsers.length > 0 ) {
+            // check for at least one selected item and check role to make sure a role is selected
+            if (!ctrl.filteredUsers || !ctrl.filteredUsers[userIndex].role) {
+                ctrl.isDisabled = true;
+            }
+            else {
+                ctrl.isDisabled = false;
+            }
+        }
+        else {
+            ctrl.isDisabled = true;
+        }
+    };
 }
