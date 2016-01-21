ea67565894c74c45a59b675b45af09ffa9dfd50b
stuart <stutech@live.com>
2016-01-21 14:22:27 -0700

    -

--- source/app/controllers/question-controller.js
+++ source/app/controllers/question-controller.js
@@ -1,14 +1,20 @@
 angular.module('QQ')
     .controller('QuestionController', QuestionController);
 
-function QuestionController($scope, $stateParams) {
+function QuestionController($scope, $stateParams, $state, $stateProvider) {
     var ctrl = this;
     ctrl.click_addPlay= click_addPlay;
+    ctrl.click_dealSubmit= click_dealSubmit;
 
-    ctrl.deal_id = $stateParams.deal_id;
+    ctrl.deal_id = $stateParams;
+    console.log(ctrl.deal_id);
 
     function click_addPlay() {
         ctrl.expand_show_addPlay = true;
         ctrl.showPlayExpanded = true;
+    }
+
+    function click_dealSubmit() {
+        $state.go('root.ask-questions', {'deal': "test"});
     }
 }
