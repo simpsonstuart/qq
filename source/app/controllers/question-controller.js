f24395e6808595c25c6ccbaefb1d717d07d5e573
stuart <stutech@live.com>
2016-01-21 16:35:52 -0700

    Added coding changes for new UI Screens

--- source/app/controllers/question-controller.js
+++ source/app/controllers/question-controller.js
@@ -1,20 +1,20 @@
 angular.module('QQ')
     .controller('QuestionController', QuestionController);
 
-function QuestionController($scope, $stateParams, $state, $stateProvider) {
+function QuestionController($scope, $state, $stateParams) {
     var ctrl = this;
     ctrl.click_addPlay= click_addPlay;
     ctrl.click_dealSubmit= click_dealSubmit;
-
-    ctrl.deal_id = $stateParams;
-    console.log(ctrl.deal_id);
+    ctrl.deal = "test";
 
     function click_addPlay() {
         ctrl.expand_show_addPlay = true;
         ctrl.showPlayExpanded = true;
     }
 
-    function click_dealSubmit() {
-        $state.go('root.ask-questions', {'deal': "test"});
+    //store the selected deal then go to the next part of ask questions
+    function click_dealSubmit(deal) {
+        console.log(deal);
+        $state.go('root.ask-questions', {deal_id: deal});
     }
 }
