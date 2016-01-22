5488913796eb1e7629fb701a474926f01daa2664
stuart <stutech@live.com>
2016-01-22 10:24:27 -0700

    -Added deal name logic to ask reps page

--- source/app/controllers/question-controller.js
+++ source/app/controllers/question-controller.js
@@ -6,6 +6,7 @@ function QuestionController($scope, $state, $stateParams) {
     ctrl.click_addPlay= click_addPlay;
     ctrl.click_questionSubmit = click_questionSubmit;
     ctrl.deal = $stateParams.deal_id;
+    var deal = ctrl.deal;
 
     function click_addPlay() {
         ctrl.expand_show_addPlay = true;
@@ -13,7 +14,8 @@ function QuestionController($scope, $state, $stateParams) {
     }
 
     //pass the dealId on to choose reps
-    function click_questionSubmit(deal) {
+    function click_questionSubmit() {
+        console.log(deal);
         $state.go('root.ask-reps', {deal_id: deal});
     }
 }
