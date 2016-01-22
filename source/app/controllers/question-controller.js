5972a12dcd73bf49a610f9b4808fe52deeb38daf
stuart <stutech@live.com>
2016-01-22 12:47:27 -0700

    -refactored naming of controllers -added logic to ask-deal pages -QQ-394: - renamed instances of quiqq to traqq

--- source/app/controllers/question-controller.js
+++ source/app/controllers/question-controller.js
@@ -5,8 +5,11 @@ function QuestionController($scope, $state, $stateParams) {
     var ctrl = this;
     ctrl.click_addPlay= click_addPlay;
     ctrl.click_questionSubmit = click_questionSubmit;
+    ctrl.click_playSelect = click_playSelect;
     ctrl.deal = $stateParams.deal_id;
+    var dealQuestion = $scope.dealQuestion;
     var deal = ctrl.deal;
+    console.log(dealQuestion);
 
     //if the deal is default or null go to the ask deal page
     if($stateParams.deal_id === "Deal ID" || $stateParams.deal_id === null){
@@ -17,6 +20,11 @@ function QuestionController($scope, $state, $stateParams) {
         ctrl.expand_show_addPlay = true;
         ctrl.showPlayExpanded = true;
     }
+
+    function click_playSelect() {
+        $state.go('root.ask-play-questions', {deal_id: deal});
+    }
+
 
     //pass the dealId on to choose reps
     function click_questionSubmit() {
