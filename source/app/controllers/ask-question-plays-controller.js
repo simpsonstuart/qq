6c0c5e7e0921b7980b7b369a7ac59388c8903db9
Jenny Ryan <jenny@pixelandline.com>
2016-01-22 16:26:01 -0700

    QQ-365 Changed logo to direct to profile/dashboard

--- source/app/controllers/ask-question-plays-controller.js
+++ source/app/controllers/ask-question-plays-controller.js
@@ -3,10 +3,16 @@ angular.module('QQ')
 
 function QuestionController($scope, $stateParams, $state) {
     var ctrl = this;
+    ctrl.click_askSubmit = click_askSubmit;
     ctrl.deal = $stateParams.deal_id;
+    var deal = ctrl.deal;
 
     //if the deal is default or null go to the ask deal page
     if(!$stateParams.deal_id || $stateParams.deal_id === "Deal ID"){
         $state.go('root.ask-deal');
+    }
+
+    function click_askSubmit() {
+        $state.go('root.ask-reps', {deal_id: deal});
     }
 }
