d2b488042e3302a843216b925faa67af2f704e5b
stuart <stutech@live.com>
2016-01-26 13:15:25 -0700

    Wired up the answer question screen

--- source/app/controllers/answer-controller.js
+++ source/app/controllers/answer-controller.js
@@ -1,7 +1,27 @@
 angular.module('QQ')
     .controller('AnswerController', AnswerController);
+     AnswerController.$inject = ['$scope'];
 
 function AnswerController($scope) {
     var ctrl = this;
+    ctrl.textAnswer = textAnswer;
+    ctrl.YesNoAnswer = YesNoAnswer;
 
+        //fictional questions
+         ctrl.questions = [
+            {"question":"Is the funding available now?", "askedBy":"Travis Jones", "questionType":"YesNo", "askedDate":"1/17/2016", "questionId":"3535"},
+            {"question":"When will sr3 be ready?", "askedBy":"Dawn Anderson", "questionType":"Text", "askedDate":"2/23/2015", "questionId":"2343"},
+            {"question":"How much is project magenta worth?", "askedBy":"David Hewitt", "questionType":"YesNo", "askedDate":"8/17/2015", "questionId":"3635"},
+            {"question":"Has proposal e3 been met in accordance to proposition 68?", "askedBy":"Jackson Davis", "questionType":"Text", "askedDate":"1/11/2016", "questionId":"897"}
+        ];
+
+    function YesNoAnswer(questionId, selection, comment) {
+
+        ctrl.answer = selection;
+        console.log(ctrl.questions);
+    }
+
+    function textAnswer(questionId, answer, comment) {
+        ctrl.answer = answer;
+    }
 }
