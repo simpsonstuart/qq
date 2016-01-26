0899c3be595c0455b609e93aa61344953d3133ca
Andy Wendt <andy@awendt.com>
2016-01-26 08:53:00 -0700

    Added warnings about no items to import; removed requirements to select items to submit form
    QQ-295, QQ-120

--- source/app/controllers/import-users-controller.js
+++ source/app/controllers/import-users-controller.js
@@ -4,21 +4,27 @@ angular.module('QQ')
 function ImportUsersController($scope, _, $state, UserService) {
     var ctrl = this;
     ctrl.submit = submit;
-    ctrl.submittable = submittable;
+    ctrl.noUsersToImport = noUsersToImport;
+    ctrl.users = [];
+    ctrl.usersRetrieved = false;
 
     UserService.importList().then(function (data) {
         ctrl.users = data;
+        ctrl.usersRetrieved = true;
     });
 
     function submit () {
         var usersToImport = _.pluck(_.filter(ctrl.users, "isChecked"), 'id');
-        console.log(usersToImport);
-        UserService.add(usersToImport).then(function (response) {
+        if (usersToImport.length > 0) {
+            UserService.add(usersToImport).then(function (response) {
+                $state.go('root.profile');
+            });
+        } else {
             $state.go('root.profile');
-        });
+        }
     }
 
-    function submittable () {
-        return !_.some(ctrl.users, 'isChecked');
+    function noUsersToImport() {
+        return ctrl.users.length < 1 && ctrl.usersRetrieved;
     }
 }
