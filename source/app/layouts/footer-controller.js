12d5c85a84a43da6010a228df2d77dbcb81aaa1c
stuart <stutech@live.com>
2016-02-05 16:46:21 -0700

    -registration confirmation page and added sync page
    -fixed navigation bugs

--- source/app/layouts/footer-controller.js
+++ source/app/layouts/footer-controller.js
@@ -10,26 +10,26 @@
 
         function activate() {
             //highlight the feed tab only by default
-            if($state.is('feed')){
-                ctrl.FeedActive = true;
+            if($state.is('settings')){
+                ctrl.HomeActive = true;
                 ctrl.DealsActive = false;
                 ctrl.NSActive = false;
-                ctrl.AskActive = false;
-            }else if ($state.is('deal-list')){
-                ctrl.FeedActive = false;
+                ctrl.SyncActive = false;
+            }else if ($state.is('deals-list')){
+                ctrl.HomeActive = false;
                 ctrl.DealsActive = true;
                 ctrl.NSActive = false;
-                ctrl.AskActive =false;
+                ctrl.SyncActive = false;
             }else if ($state.is('next-steps')){
                 ctrl.FeedActive = false;
                 ctrl.DealsActive = false;
                 ctrl.NSActive = true;
                 ctrl.AskActive =false;
-            }else if ($state.is('ask-questions')){
-                ctrl.FeedActive = false;
+            }else if ($state.is('sync')){
+                ctrl.HomeActive = false;
                 ctrl.DealsActive = false;
                 ctrl.NSActive = false;
-                ctrl.AskActive =true;
+                ctrl.SyncActive = true;
             }
         }
     }
---
+++ source/app/registration-confirmation/registration-confirmation.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+    angular.module('app.registration-confirmation')
+        .controller('RegistrationConfirmation', RegistrationConfirmation);
+
+    function RegistrationConfirmation($scope, $state, UserService) {
+        var ctrl = this;
+
+    }
+})();
