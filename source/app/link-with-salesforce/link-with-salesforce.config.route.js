ae66569f199064f0df1fcadf4157f7fe4139dda7
Jenny Ryan <jenny@pixelandline.com>
2016-02-08 11:17:35 -0700

    QQ-515 Edited html for get started and link with salesforce screens

--- source/app/link-with-salesforce/link-with-salesforce.config.route.js
+++ source/app/link-with-salesforce/link-with-salesforce.config.route.js
@@ -7,7 +7,7 @@
             url: '/link-with-salesforce',
             restricted: false,
             data: {
-                bodyClasses: 'login grey',
+                bodyClasses: 'login',
                 headerClasses: 'logo-nav',
                 footerClasses: 'nav',
                 pageTitle:     'Traqq'
