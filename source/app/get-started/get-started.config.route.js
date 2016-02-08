16a15e74b09c62c71f2f727b9c167a7132d99adc
stuart <stutech@live.com>
2016-02-08 10:32:10 -0700

    -added html and css for new get started and link with salesforce screens. -fixed styling issues

---
+++ source/app/get-started/get-started.config.route.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+    angular.module('app.registration').config(config);
+
+    function config($stateProvider) {
+        $stateProvider.state('get-started', {
+            url: '/get-started',
+            restricted: false,
+            data: {
+                bodyClasses: 'login grey',
+                headerClasses: 'logo-nav',
+                footerClasses: 'nav',
+                pageTitle:     'Traqq'
+            },
+            views: {
+                'header': {
+                    templateUrl: 'views/layouts/logo-nav.html',
+                    controller: 'HeaderController',
+                    controllerAs: 'ctrl'
+                },
+                'container': {
+                    templateUrl: 'views/get-started/get-started.html',
+                    controller: 'Registration',
+                    controllerAs: 'ctrl'
+                }
+            }
+        });
+    }
+
+})();
---
+++ source/app/link-with-salesforce/link-with-salesforce.config.route.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+    angular.module('app.link-with-salesforce').config(config);
+
+    function config($stateProvider) {
+        $stateProvider.state('link-with-salesforce', {
+            url: '/link-with-salesforce',
+            restricted: false,
+            data: {
+                bodyClasses: 'login grey',
+                headerClasses: 'logo-nav',
+                footerClasses: 'nav',
+                pageTitle:     'Traqq'
+            },
+            views: {
+                'header': {
+                    templateUrl: 'views/layouts/logo-nav.html',
+                    controller: 'HeaderController',
+                    controllerAs: 'ctrl'
+                },
+                'container': {
+                    templateUrl: 'views/link-with-salesforce/link-with-salesforce.html',
+                    controller: 'LinkWithSalesforce',
+                    controllerAs: 'ctrl'
+                }
+            }
+        });
+    }
+
+})();
