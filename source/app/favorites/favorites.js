b0ec9847d699414f8c39444e5c3e339dfe5493b7
stuart <stutech@live.com>
2016-02-09 10:01:45 -0700

    -added favorites page and created filtering logic

--- source/app/favorites/favorites.js
+++ source/app/favorites/favorites.js
@@ -3,8 +3,24 @@
     angular.module('app.favorites')
         .controller('Favorites', Favorites);
 
-    function Favorites($scope) {
+    function Favorites($scope, DealService, DateAndTimeService, NumberService) {
         var ctrl = this;
+
+        ctrl.formatDate = DateAndTimeService.formatDate;
+        ctrl.convertNumberToWord = NumberService.numberToWord;
+        ctrl.formatMoney = NumberService.formatMoney;
+        ctrl.timeToClose = DateAndTimeService.daysTill;
+        ctrl.dateNow = new Date().toJSON().slice(0,10);
+
+        activate();
+
+        function activate() {
+            ctrl.dealsForLabel = 'My Deals';
+
+            DealService.getAll().then(function (data) {
+                ctrl.favoriteDeals = _.filter(data, 'favorite');
+            });
+        }
     }
 
 })();
