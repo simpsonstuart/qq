7733241a954486b14b258759865cf69c5fed3985
stuart <stutech@live.com>
2016-02-11 15:40:42 -0700

    QQ-560 -added deal service to edit screens in order to pre populate fields with data

--- source/app/deal-detail-edit/deal-detail-edit.js
+++ source/app/deal-detail-edit/deal-detail-edit.js
@@ -3,11 +3,26 @@
     angular.module('app.deal-detail-edit')
         .controller('DealDetailEdit', DealDetailEdit);
 
-    function DealDetailEdit($scope, $stateParams, $state) {
+    function DealDetailEdit($scope, $stateParams, $state, DealService) {
         var ctrl = this;
         ctrl.cancel = cancel;
+        ctrl.save = save;
         ctrl.dealId = $stateParams.deal_id;
+
+        activate();
+
+        function activate() {
+            DealService.get(ctrl.dealId).then(function (data) {
+                ctrl.deal            = data;
+                console.log(data);
+            });
+        }
+
         function cancel () {
+            $state.go('deal-detail', {deal_id: ctrl.dealId});
+        }
+
+        function save () {
             $state.go('deal-detail', {deal_id: ctrl.dealId});
         }
     }
--- source/app/edit-close-date/edit-close-date.js
+++ source/app/edit-close-date/edit-close-date.js
@@ -3,11 +3,21 @@
     angular.module('app.edit-close-date')
         .controller('EditCloseDate', EditCloseDate);
 
-    function EditCloseDate($scope, $stateParams, $state) {
+    function EditCloseDate($scope, $stateParams, $state, DealService) {
         var ctrl = this;
         ctrl.cancel = cancel;
         ctrl.dealId = $stateParams.deal_id;
         ctrl.save = save;
+
+        activate();
+
+        function activate() {
+            DealService.get(ctrl.dealId).then(function (data) {
+                ctrl.deal            = data;
+                console.log(data);
+            });
+        }
+
         function cancel () {
             $state.go('deal-detail', {deal_id: ctrl.dealId});
         }
