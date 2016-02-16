36a043479f537c6a3835b4016d48ba2dce124e0f
stuart <stutech@live.com>
2016-02-16 11:58:12 -0700

    QQ-575 -added logic to handle error success and and confirmation states. Also added timer function to prevent spam

--- source/app/registration-confirmation/registration-confirmation.js
+++ source/app/registration-confirmation/registration-confirmation.js
@@ -3,22 +3,50 @@
     angular.module('app.registration-confirmation')
         .controller('RegistrationConfirmation', RegistrationConfirmation);
 
-    function RegistrationConfirmation($state, UserService, UrlService) {
+    function RegistrationConfirmation($state, UserService, UrlService, $timeout) {
         var returnUrl = UrlService.urlWithoutPath() + '/get-started';
 
         var ctrl = this;
         ctrl.verificationResent = false;
         ctrl.sendError = false;
         ctrl.resendVerification = resendVerification;
+        resendWait();
 
         function resendVerification() {
             UserService.resendVerificationEmail($state.params.email, returnUrl).then(function () {
-                ctrl.verificationResent = true;
+                ctrl.verificationSent = true;
+                ctrl.resendEnabled = false;
+                ctrl.verificationSent = true;
+                resendWait();
             }, function () {
                 ctrl.sendError = true;
+                ctrl.verificationSent = false;
+                ctrl.resendEnabled = false;
+                resendWait();
+
                 console.log('error sending verification email')
             });
         }
 
-    }
+        //makes the user wait 5 seconds before being able to resend email
+        function resendWait() {
+            ctrl.countDown = 6;
+            runCounter();
+
+            function runCounter() {
+                ctrl.showCountdown = true;
+                ctrl.countDown -= 1;
+                if ( ctrl.countDown > 0)
+                    $timeout(runCounter, 1000);
+
+                if(ctrl.countDown <= 0) {
+                    ctrl.resendEnabled = true;
+                    ctrl.showCountdown = false;
+                    ctrl.sendError = false;
+                }
+            }
+
+
+            }
+        }
 })();
