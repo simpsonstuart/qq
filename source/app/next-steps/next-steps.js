c7d383bd2a08b7feaee80a23ceb73211ffa71058
Andy Wendt <andy@awendt.com>
2016-02-15 19:17:09 -0700

    next steps displaying and filtering
    QQ-502

--- source/app/next-steps/next-steps.js
+++ source/app/next-steps/next-steps.js
@@ -2,46 +2,54 @@
     'use strict';
     angular.module('app.next-steps').controller('NextSteps',NextSteps);
 
-    function NextSteps(DealService, $scope, $location, NumberService, _) {
+    function NextSteps(DealService, NumberService, _) {
         var ctrl = this;
         ctrl.filterNextSteps = filterNextSteps;
         ctrl.formatMoney = NumberService.formatMoney;
         ctrl.filterActive = false;
         ctrl.selectedSort = 'close_date';
 
-        activate();
-
-        function activate() {
-            DealService.getAll('include=owner').then(function (data) {
-                ctrl.deals = data;
-            });
-        }
-
-        //filters deals based on if they have next steps or not filtering for other types happens in angular
-        ctrl.filteredDeals = _.filter(ctrl.deals, "next_step");
-
-        if(ctrl.filteredDeals.length < 1) {
-
-            ctrl.showNoDealsWithoutNextSteps = true;
-        }
+        _activate();
 
         function filterNextSteps(){
             ctrl.filterActive= !ctrl.filterActive;
             if(ctrl.filterActive){
-
-                ctrl.filteredDeals  = _.reject(ctrl.deals, "next_step");
+                ctrl.filteredDeals  = _.reject(ctrl.deals, _doesNotHaveNextStepFilter);
                 ctrl.noNextStep = true;
                 ctrl.showNoDealsWithoutNextSteps = false;
-            }
-            else{
-
-                ctrl.filteredDeals = _.filter(ctrl.deals, "next_step");
+            } else {
+                ctrl.filteredDeals = _.filter(ctrl.deals, _hasNextStepFilter);
                 ctrl.noNextStep = false;
-                if(ctrl.filteredDeals.length < 1) {
 
+                if(ctrl.filteredDeals.length < 1) {
                     ctrl.showNoDealsWithoutNextSteps = true;
                 }
             }
+        }
+
+
+        function _activate() {
+            DealService.getAll('include=owner').then(function (data) {
+                ctrl.deals = data;
+
+                //filters deals based on if they have next steps or not filtering for other types happens in angular
+                ctrl.filteredDeals = _.filter(ctrl.deals, _hasNextStepFilter);
+
+                if(ctrl.filteredDeals.length < 1) {
+                    ctrl.showNoDealsWithoutNextSteps = true;
+                }
+            });
+        }
+
+        function _hasNextStepFilter (deal) {
+            if (deal.next_step == null) {
+                return false;
+            }
+            return !!deal.next_step.trim();
+        }
+
+        function _doesNotHaveNextStepFilter(deal) {
+            return ! _hasNextStepFilter(deal);
         }
     }
 })();
