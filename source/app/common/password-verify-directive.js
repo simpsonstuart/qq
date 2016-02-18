b22a573e6b2e55de95aa46dcb2172733a160ffc6
stuart <stutech@live.com>
2016-02-18 12:11:29 -0700

    -fixed broken hamburger menu -fixed bugs in registration styling

--- source/app/common/password-verify-directive.js
+++ source/app/common/password-verify-directive.js
@@ -1,35 +1,18 @@
 (function () {
     'use strict';
-    angular.module('app.common')
-        .directive("passwordVerify", passwordVerify);
 
-    function passwordVerify() {
+    angular.module('app.common').directive('pwCheck', pwCheck);
+
+    function pwCheck() {
         return {
-            require: "ngModel",
-            scope: {
-                passwordVerify: '='
-            },
-            link: function(scope, element, attrs, ctrl) {
-                scope.$watch(function() {
-                    var combined;
-
-                    if (scope.passwordVerify || ctrl.$viewValue) {
-                        combined = scope.passwordVerify + '_' + ctrl.$viewValue;
-                    }
-                    return combined;
-                }, function(value) {
-                    if (value) {
-                        ctrl.$parsers.unshift(function(viewValue) {
-                            var origin = scope.passwordVerify;
-                            if (origin !== viewValue) {
-                                ctrl.$setValidity("passwordVerify", false);
-                                return undefined;
-                            } else {
-                                ctrl.$setValidity("passwordVerify", true);
-                                return viewValue;
-                            }
-                        });
-                    }
+            require: 'ngModel',
+            link: function (scope, elem, attrs, ctrl) {
+                var firstPassword = '#' + attrs.pwCheck;
+                elem.add(firstPassword).on('keyup', function () {
+                    scope.$apply(function () {
+                        // console.info(elem.val() === $(firstPassword).val());
+                        ctrl.$setValidity('pwmatch', elem.val() === $(firstPassword).val());
+                    });
                 });
             }
         };
