004f7c2b6a9ca57c7adba3b023c06d75daa713c9
stuart <stutech@live.com>
2016-02-19 15:09:08 -0700

    QQ-590 -fixed bugs with email validation and other issues with registration page

---
+++ source/app/common/email-verify-directive.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+
+    angular.module('app.common').directive('emailVerify', pwCheck);
+    function pwCheck() {
+        var INTEGER_REGEXP = new RegExp('^[a-z0-9]+(\.[_a-z0-9]+)*@@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,50})$', 'i');
+        return {
+            require: 'ngModel',
+            link: function (scope, elm, attrs, ctrl) {
+                ctrl.$parsers.unshift(function (viewValue) {
+                    if (INTEGER_REGEXP.test(viewValue)) {
+                        // it is valid
+                        ctrl.$setValidity('emailVerify', true);
+                        return viewValue;
+                    } else {
+                        // it is invalid, return undefined (no model update)
+                        ctrl.$setValidity('emailVerify', false);
+                        return undefined;
+                    }
+                });
+            }
+        };
+    }
+
+})();
--- source/app/registration/registration.js
+++ source/app/registration/registration.js
@@ -4,16 +4,30 @@
         .controller('Registration', Registration);
 
     function Registration($scope, $state, UserService, UrlService) {
-        var ctrl = this;
-        var returnUrl = UrlService.urlWithoutPath() + '/get-started';
-        ctrl.register = register;
+        var ctrl         = this;
+        var returnUrl    = UrlService.urlWithoutPath() + '/get-started';
+        ctrl.register    = register;
+        ctrl.emailVerify = emailVerify;
 
         function register() {
             UserService.register((function () {
-                return { 'email': $scope.email, 'password': $scope.pw2, 'return_url': returnUrl};
-            })()).then(function(){
+                return {'email': $scope.email, 'password': $scope.pw2, 'return_url': returnUrl};
+            })()).then(function () {
                 $state.go('registration-confirmation', {email: $scope.email});
             });
         }
+
+        function emailVerify() {
+            var reg = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,63}$/;
+            if (reg.test($scope.email)){
+
+                ctrl.emailInvalid = false;
+            }
+            else{
+                ctrl.emailInvalid = true;
+                $scope.registration_form.$invalid = true;
+            }
+        }
+
     }
 })();
