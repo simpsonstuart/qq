b5469896d369f4cf8cfba394d1383687daaf09e9
Andy Wendt <andy@awendt.com>
2016-02-23 15:34:25 -0700

    Fixed issue with boolean instead of string comparison for account validation
    QQ-618

--- source/app/get-started/get-started.js
+++ source/app/get-started/get-started.js
@@ -5,18 +5,26 @@
 
     function GetStarted($scope, $state, UserService) {
         var ctrl = this;
+        ctrl.error = false;
+        ctrl.showGetStarted = false;
+        ctrl.verifying = false;
         ctrl.getStarted = getStarted;
 
         activate();
 
         function activate() {
             var token = $state.params.token;
-            if (token == "false") {
+            ctrl.verifying = true;
+
+            if (token == "verified") {
                 $state.go('login');
             } else {
                 UserService.verify(token).then(function () {
+                    ctrl.verifying = false;
                     ctrl.showGetStarted = true;
                 }, function () {
+                    ctrl.verifying = false;
+                    ctrl.error = true;
                     console.log('error token invalid')
                 });
             }
