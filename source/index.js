9f669dbc3542e156787dea770d4b3b8ab1ddf240
Andy Wendt <andy@awendt.com>
2016-03-03 11:15:39 -0700

    added deep linking; made base tag conditional on web

---
+++ source/index.js
@@ -0,0 +1,22 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    // Bind Event Listeners
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    // deviceready Event Handler
+    onDeviceReady: function() {
+        universalLinks.subscribe('ul_Traqq', app.didLaunchAppFromLink);
+    },
+
+    didLaunchAppFromLink: function(eventData) {
+        alert('Did launch application from the link: ' + eventData);
+    }
+};
+
+app.initialize();
